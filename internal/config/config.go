96266a44481bd2e8fdabfc825f4b83cf39534f3c
Md. Mahim Talukder <mahim4163@gmail.com>
2026-01-13 17:38:32 +0600

    initialize config package and add local configuration file

---
+++ internal/config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Address string
+}
+type Config struct {
+	Env         string `yaml:"env" env-required:"true" env-default:"production"` //it called struct tag
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to config file")
+		flag.Parse()
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config path is not set yet")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("Can't read config: %s", err.Error())
+	}
+
+	return &cfg
+}
